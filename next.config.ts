1a1ecc577bb60b860df66bad7966152831c15538
raiden <alicewolffernandes@gmail.com>
2026-05-18 15:53:08 -0300

    chore: setup do launch do github pages

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: process.env.GITHUB_ACTIONS ? "export" : undefined,
+  basePath: process.env.GITHUB_ACTIONS ? "/my-portfolio" : "",
   images: {
     unoptimized: true,
   },
